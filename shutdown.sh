63606ea4e2847a5e351efd386d45b90c4a372443
Verduranti <angelofchaos@gmail.com>
2015-04-28 03:12:46 +0000

    Works now!

--- shutdown.sh
+++ shutdown.sh
@@ -3,6 +3,10 @@
 #Shuts down the camera
 #Disables wifi direct
 
+wpa_cli -ip2p-dev-wlan0 p2p_stop_find
+wpa_cli p2p_group_remove `wpa_cli interface | grep 'p2p-wlan0'`
+
+
 #(server.js) and 
 #nohup node server.js >> "$stdout_log" 2>> "$stderr_log" &
 
